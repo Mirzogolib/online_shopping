a146b566f522cd69a850e90dd6d3f5d45f25ce38
mirzogolib <mirzogolib96@gmail.com>
2020-10-27 10:11:48 +0500

    Cart item changed

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -49,7 +49,7 @@ class ProductItem extends StatelessWidget {
             ),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
-              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).hideCurrentSnackBar(); 
               Scaffold.of(context).showSnackBar(
                 SnackBar(
                   content: Text(
