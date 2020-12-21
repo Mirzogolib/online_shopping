f2355b6a5269f967a39df5e8da8342741d32fce6
mirzogolib <mirzogolib96@gmail.com>
2020-12-21 16:22:12 +0500

    Fetching order from Firebase done

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -4,7 +4,27 @@ import 'package:online_shopping/widgets/app_drawer.dart';
 import 'package:online_shopping/widgets/order_item.dart';
 import 'package:provider/provider.dart';
 
-class OrdersPage extends StatelessWidget {
+class OrdersPage extends StatefulWidget {
+  @override
+  _OrdersPageState createState() => _OrdersPageState();
+}
+
+class _OrdersPageState extends State<OrdersPage> {
+  var _isLoading = false;
+  @override
+  void initState() {
+    Future.delayed(Duration.zero).then((_) async {
+      setState(() {
+        _isLoading = true;
+      });
+      await Provider.of<OrderProvider>(context, listen: false).fetchOrders();
+      setState(() {
+        _isLoading = false;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final ordersProvider = Provider.of<OrderProvider>(context);
@@ -15,12 +35,14 @@ class OrdersPage extends StatelessWidget {
         ),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
-        itemBuilder: (ctx, i) => OrderItem(
-          ordersProvider.orders[i],
-        ),
-        itemCount: ordersProvider.orders.length,
-      ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemBuilder: (ctx, i) => OrderItem(
+                ordersProvider.orders[i],
+              ),
+              itemCount: ordersProvider.orders.length,
+            ),
     );
   }
 }
--- lib/providers/orders_provider.dart
+++ lib/providers/orders_provider.dart
@@ -4,11 +4,45 @@ import 'package:online_shopping/models/cart.dart';
 import 'package:online_shopping/models/order.dart';
 import 'package:http/http.dart' as http;
 import 'package:online_shopping/tools/constants.dart';
+import 'package:online_shopping/widgets/order_item.dart';
 
 class OrderProvider with ChangeNotifier {
   List<Order> _orders = [];
   List<Order> get orders {
     return [..._orders];
+  }
+
+  Future<void> fetchOrders() async {
+    final url = Constants.mainAPI + 'orders.json';
+    final response = await http.get(url);
+    final List<Order> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if (extractedData == null) {
+      return;
+    }
+    extractedData.forEach((orderId, orderItem) {
+      loadedOrders.add(
+        Order(
+          id: orderId,
+          amount: orderItem['amount'],
+          dateTime: DateTime.parse(
+            orderItem['dateTime'],
+          ),
+          products: (orderItem['products'] as List<dynamic>)
+              .map(
+                (e) => CartItem(
+                    id: e['id'],
+                    title: e['title'],
+                    quantity: e['quantity'],
+                    price: e['price']),
+              )
+              .toList(),
+        ),
+      );
+    });
+
+    _orders = loadedOrders;
+    notifyListeners();
   }
 
   Future<void> addOrder(List<CartItem> cartProducts, double totalPrice) async {
